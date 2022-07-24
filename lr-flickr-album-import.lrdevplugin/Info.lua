fb60a70a404c2dedec59aa6868c52b70155144bd
Andrew Craig <andrewsc@gmail.com>
2022-07-24 14:50:53 +0100

    Initial Commit

---
+++ lr-flickr-album-import.lrdevplugin/Info.lua
@@ -0,0 +1,12 @@
+return {
+    LrSdkVersion = 11.0,
+    LrToolkitIdentifier = 'dev.acraig.lr.flickrimport',
+    LrPluginName = LOC "$$$/FlickrImport/PluginName=Import Flickr Albums",
+    LrLibraryMenuItems = {
+        {
+            title = LOC "$$$/FlickrImport/LaunchDialog=Import Flickr Dump Albums",
+            file = "LaunchFlickrDumpDialog.lua"
+        }
+    },
+    VERSION = { major = 1, minor = 0, build="20220622001"}
+}
